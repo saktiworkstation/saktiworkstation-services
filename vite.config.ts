03fa150f7ea69543083ab390441cf545a094427f
saktiworkstation <saktiworkstation@gmail.com>
2025-01-28 21:14:45 +0700

    Fixed

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,12 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
